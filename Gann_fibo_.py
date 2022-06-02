741c54311b6886c3e069e536a1c13e83e7abbb27
ramakrishnamekala129 <ramakrishnamekala129@gmail.com>
2022-06-02 13:35:53 +0530

    Update Gann_fibo_.py

--- Gann_fibo_.py
+++ Gann_fibo_.py
@@ -454,11 +454,11 @@ if gann:
             df1=df1.reset_index()
             df1=df1.iloc[:,1:]
     df1=df1.div(Divide)
-    if Divide == 1 :
-    	st.dataframe(df1.style.format("{:.2f}"))
-    elif Divide == 1000 :
-    	st.dataframe(df1.style.format("{:.3f}"))
+	if Divide == 1 :
+		st.dataframe(df1.style.format("{:.2f}"))
+	elif Divide == 1000 :
+		st.dataframe(df1.style.format("{:.3f}"))
 	elif Divide == 10000 :
-    	st.dataframe(df1.style.format("{:.5f}"))
-    else:
-    	st.dataframe(df1.style.format("{:.2f}"))
+		st.dataframe(df1.style.format("{:.5f}"))
+	else:
+		st.dataframe(df1.style.format("{:.2f}"))
